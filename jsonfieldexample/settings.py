4bc3b6ab8c917abe7c6c73c3816086e1e7a88557
Roopa <sejekanroopa0@gmail.com>
2022-12-09 20:28:47 +0530

    updated new language and tested

--- jsonfieldexample/settings.py
+++ jsonfieldexample/settings.py
@@ -15,6 +15,16 @@ from pathlib import Path
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+import environ
+
+env = environ.Env()
+# reading .env file
+environ.Env.read_env()
+
+# Raises django's ImproperlyConfigured exception if SECRET_KEY not in os.environ
+# SECRET_KEY = env("SECRET_KEY")
+#SECRET_KEY = env("SECRET_KEY", default="unsafe-secret-key")
+
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
@@ -75,6 +85,16 @@ WSGI_APPLICATION = 'jsonfieldexample.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql_psycopg2',
+#         'NAME': env("DATABASE_NAME"),
+#         'USER': env("DATABASE_USER"),
+#         'PASSWORD': env("DATABASE_PASSWORD"),
+#         'HOST': env("DATABASE_HOST"),
+#         'PORT': env("DATABASE_PORT")
+#     }
+# }
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
@@ -112,6 +132,8 @@ AUTH_PASSWORD_VALIDATORS = [
 LANGUAGES = [
  ('en','English'),
  ('fr', 'French'),
+ ('hi', 'Hindi'),
+
 
 ]
 
